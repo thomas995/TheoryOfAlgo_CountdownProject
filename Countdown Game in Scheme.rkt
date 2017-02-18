4d109760afb28f0f5d9330fa4c433361e3e32d97
thomas995 <G00314495@gmit.ie>
2017-02-18 18:57:07 +0000

    sample 'user input' for adding all the list values

--- Countdown Game in Scheme.rkt
+++ Countdown Game in Scheme.rkt
@@ -5,7 +5,6 @@
 
 ;4 SMALL NUMBERS
 'SMALL
-'
 (define SmallList1(list-ref (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10) (random 20)))   ; Defines a variable SmallList that randomly generates a number from one to ten
 SmallList1 ; outputs the generated value to the console
 
@@ -20,7 +19,6 @@ SmallList4 ; outputs the generated value to the console
 
 ;2 LARGE NUMBERS
 'LARGE
-'
 (define LargeList1(list-ref(list 25 50 75 100) (random 4))) ; Defines a variable LargeList that randomly generates a number from the list
 LargeList1 ; outputs the generated value to the console
 
@@ -29,8 +27,23 @@ LargeList2 ; outputs the generated value to the console
 
 ;TARGET NUMBER
 'TARGET
-'
 (define TargetRange(+ 101 (random 999))) ; Generates a random value from 101 to 999 that will be used later as a target to reach
 TargetRange ; outputs the generated value to the console
+
+'_____________________________________
+
+; Adds all the randomly selected numbers from the list to get an "attempt answer"
+(define attemptAnswer(+ SmallList1 SmallList2 SmallList3 SmallList4 LargeList1 LargeList2))
+attemptAnswer ; outputs the generated value to the console
+
+;IS THE ATTEMPTED ANSWER CORRECT
+(let ([arg1 attemptAnswer] [arg2 TargetRange]) ; assigns 2 values created earlier as arguments
+  (cond ; the condition of the if else statement
+    [(< arg1 arg2) (display "Attempt Is Too Low")] ; if the first argument is less than the second, say the attempt is too low
+    [(> arg1 arg2) (display arg1) (newline) (display "Attempt Is Too High")] ; if the first argument is more than the second, say the attempt is too low
+    [else (display arg2) (newline) (display "Attempt Is Correct!")])) ; else, if the attemp is correct, then display that the attempt was correct
+
+
+
 
 
